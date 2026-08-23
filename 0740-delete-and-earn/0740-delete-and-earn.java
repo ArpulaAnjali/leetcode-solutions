562c0bb8b944992d72a93fbd9f335d4012911770
Anjali Arpula <179099494+ArpulaAnjali@users.noreply.github.com>
2026-08-23 20:04:14 +0530

    Time: 14 ms (11.28%), Space: 46.2 MB (96.86%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        Map<Integer, Integer> points = new HashMap<>();
+        for (int num : nums) {
+            points.put(num, points.getOrDefault(num, 0) + num);
+        }
+        int maxValue = Arrays.stream(nums).max().getAsInt();
+        int[] dp = new int[maxValue + 1];
+        dp[0] = 0;
+        dp[1] = points.getOrDefault(1, 0);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + points.getOrDefault(i, 0), dp[i - 1]);
+        }
+        return dp[maxValue];
+    }
+}
