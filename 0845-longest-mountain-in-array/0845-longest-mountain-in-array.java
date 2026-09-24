7a85226663ea7cbf87341df1c5463bd7f93d2203
Anjali Arpula <179099494+ArpulaAnjali@users.noreply.github.com>
2026-09-24 13:50:15 +0530

    Time: 2 ms (99.81%), Space: 47.6 MB (18.13%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int longestMountain(int[] arr) {
       int n=arr.length;
-      if(n<3){
-        return 0;
-      }
+      //if(n<3){
+        //return 0;
+      //}
       int max=0;
       for(int i=1;i<n-1;i++){
         if(arr[i]>arr[i+1]&&arr[i]>arr[i-1]){
@@ -13,7 +13,7 @@ class Solution {
                 l--;
             }
             while(r<n-1&&arr[r+1]<arr[r]){
-                r++;
+                r++;  
             }
             max=Math.max(max,r-l+1);
             i=r;
